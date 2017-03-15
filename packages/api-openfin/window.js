3ebfa2609426f66cfdb47af248f64e21f6e0b701
Ben Lambert <blambert@scottlogic.com>
2017-03-15 13:52:00 +0000

    Add child window support for OpenFin

--- packages/api-openfin/window.js
+++ packages/api-openfin/window.js
@@ -2,4 +2,30 @@ if (!window.ssf) {
   window.ssf = {};
 }
 
-window.ssf.window = (...args) => window.open(...args);
+window.ssf.Window = function(url, name, features) {
+  let newWindow;
+
+  const handleError = (error) => {
+    console.error('Error creating window: ' + error);
+  };
+
+  const featureArray = features.split(',');
+
+  if (featureArray.includes('child=yes')) {
+    newWindow = new fin.desktop.Window({
+      name,
+      url
+    }, () => newWindow.show(), handleError);
+  } else {
+    newWindow = new fin.desktop.Application({
+      name,
+      url,
+      uuid: name,
+      mainWindowOptions: {
+        autoShow: true
+      }
+    }, () => newWindow.run(), handleError).getWindow();
+  }
+
+  return newWindow;
+};
