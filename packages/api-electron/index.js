82190a506b5a8d1c792a400d68d77e57d6e0b71b
Ben Lambert <blambert@scottlogic.com>
2017-03-17 08:13:48 +0000

    Add child window support (#20)

--- packages/api-electron/index.js
+++ packages/api-electron/index.js
@@ -20,15 +20,41 @@ module.exports = () => {
   });
 
   ipc.on('ssf-new-window', (e, msg) => {
-    const newWindow = new BrowserWindow();
+    const options = {};
+    const featureObject = parseFeaturesString(msg.features);
+    if (featureObject.child) {
+      options.parent = BrowserWindow.fromWebContents(e.sender);
+    }
+
+    const newWindow = new BrowserWindow(options);
     newWindow.loadURL(msg.url);
-    windows.push(newWindow);
     newWindow.on('close', () => {
       const index = windows.indexOf(newWindow);
       if (index >= 0) {
         windows.splice(index, 1);
       }
     });
+
     e.returnValue = newWindow;
+    windows.push(newWindow);
   });
+};
+
+const parseFeaturesString = (features) => {
+  const featureObject = {};
+
+  features.split(',').forEach((feature) => {
+    let [key, value] = feature.split('=');
+
+    // interpret the value as a boolean, if possible
+    if (value === 'yes' || value === '1') {
+      value = true;
+    } else if (value === 'no' || value === '0') {
+      value = false;
+    }
+
+    featureObject[key] = value;
+  });
+
+  return featureObject;
 };
--- packages/api-openfin/window.js
+++ packages/api-openfin/window.js
@@ -2,4 +2,52 @@ if (!window.ssf) {
   window.ssf = {};
 }
 
-window.ssf.window = (...args) => window.open(...args);
+window.ssf.window = function(url, name, features) {
+  let newWindow;
+  const featureObject = parseFeaturesString(features);
+  const handleError = (error) => console.error('Error creating window: ' + error);
+
+  if (featureObject.child) {
+    newWindow = new fin.desktop.Window({
+      name,
+      url
+    }, () => newWindow.show(), handleError);
+  } else {
+    // UUID must be the same as name
+    const uuid = name;
+    const mainWindowOptions = {
+      autoShow: true
+    };
+
+    const app = new fin.desktop.Application({
+      name,
+      url,
+      uuid,
+      mainWindowOptions
+    }, () => app.run(), handleError);
+
+    // Need to return the window object, not the application
+    newWindow = app.getWindow();
+  }
+
+  return newWindow;
+};
+
+const parseFeaturesString = (features) => {
+  const featureObject = {};
+
+  features.split(',').forEach((feature) => {
+    let [key, value] = feature.split('=');
+
+    // interpret the value as a boolean, if possible
+    if (value === 'yes' || value === '1') {
+      value = true;
+    } else if (value === 'no' || value === '0') {
+      value = false;
+    }
+
+    featureObject[key] = value;
+  });
+
+  return featureObject;
+};
