2c3fbc8d1a3d75d66f52e9e7a0191eede53c8a36
Ben Lambert <blambert@scottlogic.com>
2017-03-21 16:42:51 +0000

    Add messaging demo to api-specification

---
+++ packages/api-specification/src/messaging-api.js
@@ -0,0 +1,26 @@
+const messageBox = document.getElementById('message-box');
+const sendButton = document.getElementById('send-message');
+const newWindowButton = document.getElementById('new-window');
+
+const windowDetailsId = document.getElementById('window-uuid');
+windowDetailsId.innerText = ssf.Window.getCurrentWindowId();
+
+newWindowButton.onclick = () => {
+  // Create a random hex string as the window name
+  const id = (((1 + Math.random()) * 0x1000000) | 0).toString(16).substring(1);
+
+  const isChild = document.getElementById('child').checked ? 'yes' : 'no';
+
+  // eslint-disable-next-line no-new
+  new ssf.Window('http://localhost:5000/messaging-api-test-window.html', id, 'child=' + isChild);
+
+  ssf.MessageService.subscribe('*', 'test', (message, senderId) => {
+    messageBox.innerText = '\'' + message + '\' from ' + senderId;
+  });
+};
+
+sendButton.onclick = () => {
+  const uuid = document.getElementById('uuid').value;
+  const message = document.getElementById('message').value;
+  ssf.MessageService.send(uuid, 'test', message);
+};
