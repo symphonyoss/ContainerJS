f85f031cacf9cf345ac59971c3f2162b5b29ba2f
Ben Lambert <blambert@scottlogic.com>
2017-03-27 08:13:10 +0100

    Make electron api consistent with other APIs

--- packages/api-electron/src/preload/window.js
+++ packages/api-electron/src/preload/window.js
@@ -2,23 +2,23 @@ const ipc = require('electron').ipcRenderer;
 
 class Window {
   constructor(url, name, features) {
-    this.id = ipc.sendSync('ssf-new-window', {
+    this.innerWindow = ipc.sendSync('ssf-new-window', {
       url,
       name,
       features
-    }).id;
+    });
   }
 
   close() {
-    ipc.send('ssf-close-window', this.id);
+    ipc.send('ssf-close-window', this.innerWindow.id);
   }
 
   show() {
-    ipc.send('ssf-show-window', this.id);
+    ipc.send('ssf-show-window', this.innerWindow.id);
   }
 
   hide() {
-    ipc.send('ssf-hide-window', this.id);
+    ipc.send('ssf-hide-window', this.innerWindow.id);
   }
 
   static getCurrentWindowId() {
