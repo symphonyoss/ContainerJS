0e9e47ee04705caa910c658b2ddeab91c08e1ed1
Ben Lambert <blambert@scottlogic.com>
2017-03-27 16:38:41 +0100

    Moved appReady to encapsulate whole file

--- packages/api-specification/src/messaging-api-demo.js
+++ packages/api-specification/src/messaging-api-demo.js
@@ -4,27 +4,27 @@ const newWindowButton = document.getElementById('new-window');
 
 const appReady = ssf.app.ready();
 
-const windowDetailsId = document.getElementById('window-uuid');
-windowDetailsId.innerText = ssf.Window.getCurrentWindowId();
+appReady.then(() => {
+  const windowDetailsId = document.getElementById('window-uuid');
+  windowDetailsId.innerText = ssf.Window.getCurrentWindowId();
 
-newWindowButton.onclick = () => {
-  // Create a random hex string as the window name
-  const id = (((1 + Math.random()) * 0x1000000) | 0).toString(16).substring(1);
+  newWindowButton.onclick = () => {
+    // Create a random hex string as the window name
+    const id = (((1 + Math.random()) * 0x1000000) | 0).toString(16).substring(1);
 
-  const isChild = document.getElementById('child').checked ? 'yes' : 'no';
+    const isChild = document.getElementById('child').checked ? 'yes' : 'no';
 
-  appReady.then(() => {
     // eslint-disable-next-line no-new
     new ssf.Window(`http://localhost:${location.port}/messaging-api-test-window.html`, id, 'child=' + isChild);
 
     ssf.MessageService.subscribe('*', 'test', (message, senderId) => {
       messageBox.innerText = '\'' + message + '\' from ' + senderId;
     });
-  });
-};
+  };
 
-sendButton.onclick = () => {
-  const uuid = document.getElementById('uuid').value;
-  const message = document.getElementById('message').value;
-  ssf.MessageService.send(uuid, 'test', message);
-};
+  sendButton.onclick = () => {
+    const uuid = document.getElementById('uuid').value;
+    const message = document.getElementById('message').value;
+    ssf.MessageService.send(uuid, 'test', message);
+  };
+});
--- packages/api-specification/src/window-api-demo.js
+++ packages/api-specification/src/window-api-demo.js
@@ -4,43 +4,43 @@ const appReady = ssf.app.ready();
 
 let win;
 
-newWindowButton.onclick = function() {
-  var url = document.getElementById('url').value;
-  var windowName = document.getElementById('name').value;
-  var isChild = document.getElementById('child').checked;
-  appReady.then(() => {
+appReady.then(() => {
+  newWindowButton.onclick = function() {
+    var url = document.getElementById('url').value;
+    var windowName = document.getElementById('name').value;
+    var isChild = document.getElementById('child').checked;
     win = new ssf.Window(url, windowName, {
       child: isChild
     });
-  });
-};
+  };
 
-var closeWindow = document.getElementById('close-window');
+  var closeWindow = document.getElementById('close-window');
 
-closeWindow.onclick = () => {
-  win.close();
-};
+  closeWindow.onclick = () => {
+    win.close();
+  };
 
-var hideWindow = document.getElementById('hide-window');
+  var hideWindow = document.getElementById('hide-window');
 
-hideWindow.onclick = () => {
-  win.hide();
-};
+  hideWindow.onclick = () => {
+    win.hide();
+  };
 
-var showWindow = document.getElementById('show-window');
+  var showWindow = document.getElementById('show-window');
 
-showWindow.onclick = () => {
-  win.show();
-};
+  showWindow.onclick = () => {
+    win.show();
+  };
 
-var focusWindow = document.getElementById('focus-window');
+  var focusWindow = document.getElementById('focus-window');
 
-focusWindow.onclick = () => {
-  win.focus();
-};
+  focusWindow.onclick = () => {
+    win.focus();
+  };
 
-var blurWindow = document.getElementById('blur-window');
+  var blurWindow = document.getElementById('blur-window');
 
-blurWindow.onclick = () => {
-  win.blur();
-};
+  blurWindow.onclick = () => {
+    win.blur();
+  };
+});
