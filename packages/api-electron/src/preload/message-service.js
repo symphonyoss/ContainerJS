3631fc0c1bb1c6087fec648d61186cd5d15e5608
Ben Lambert <blambert@scottlogic.com>
2017-03-27 16:35:29 +0100

    Add unsubscribe for message service

--- packages/api-electron/src/preload/message-service.js
+++ packages/api-electron/src/preload/message-service.js
@@ -1,5 +1,7 @@
 const ipc = require('electron').ipcRenderer;
 
+const listenerMap = new Map();
+
 class MessageService {
   static send(windowId, topic, message) {
     ipc.send('ssf-send-message', {
@@ -10,12 +12,36 @@ class MessageService {
   }
 
   static subscribe(windowId, topic, listener) {
-    ipc.on(`ssf-send-message-${topic}`, (message, sender) => {
+    const receiveMessage = (message, sender) => {
       // Check this was from the correct window
-      if (windowId === sender || windowId === '*') {
+      if (windowId === sender.toString() || windowId === '*') {
         listener(message, sender);
       }
+    };
+
+    ipc.on(`ssf-send-message-${topic}`, receiveMessage);
+
+    // Map the arguments to the actual listener that was added
+    listenerMap.set({
+      windowId,
+      topic,
+      listener
+    }, receiveMessage);
+  }
+
+  static unsubscribe(windowId, topic, listener) {
+    let deleteKey = null;
+
+    // We cant use listenerMap.has() here becuase reconsrtucting the key from the arguments is a different object
+    // i.e. {} !== {}
+    listenerMap.forEach((value, key) => {
+      if (key.windowId === windowId && key.topic === topic && key.listener === listener) {
+        ipc.removeListener(`ssf-send-message-${topic}`, value);
+        deleteKey = key;
+      }
     });
+
+    listenerMap.delete(deleteKey);
   }
 }
 
