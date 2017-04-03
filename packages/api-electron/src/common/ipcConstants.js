b40915eed8ff3687442cbbafbbb3ef31d6335fe8
Ben Lambert <blambert@scottlogic.com>
2017-04-03 08:35:57 +0100

    Add constants for electron ipc

---
+++ packages/api-electron/src/common/ipcConstants.js
@@ -0,0 +1,14 @@
+// Add new constants in alphabetical order
+exports.IPC_SSF_BLUR_WINDOW = 'ssf-blur-window';
+exports.IPC_SSF_CAPTURE_SCREEN_SNIPPET = 'ssf-capture-screen-snippet';
+exports.IPC_SSF_CLOSE_WINDOW = 'ssf-close-window';
+exports.IPC_SSF_FOCUS_WINDOW = 'ssf-focus-window';
+exports.IPC_SSF_GET_WINDOW_ID = 'ssf-get-window-id';
+exports.IPC_SSF_HIDE_WINDOW = 'ssf-hide-window';
+exports.IPC_SSF_NEW_WINDOW = 'ssf-new-window';
+exports.IPC_SSF_NOTIFICATION = 'ssf-notification';
+exports.IPC_SSF_SCREEN_SNIPPET_CAPTURED = 'ssf-screen-snippet-captured';
+exports.IPC_SSF_SEND_MESSAGE = 'ssf-send-message';
+exports.IPC_SSF_SHOW_WINDOW = 'ssf-show-window';
+exports.IPC_SSF_WINDOW_EVENT = 'ssf-window-event';
+exports.IPC_SSF_WINDOW_SUBSCRIBE_EVENTS = 'ssf-window-subscribe-events';
