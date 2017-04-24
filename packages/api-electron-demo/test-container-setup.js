53ca752fbf95ae2669cde2a823a7931b93e93898
Ben Lambert <blambert@scottlogic.com>
2017-04-24 09:25:50 +0100

    Add CI tests on appveyor

--- packages/api-electron-demo/test-container-setup.js
+++ packages/api-electron-demo/test-container-setup.js
@@ -1,11 +1,12 @@
 const Application = require('spectron').Application;
 const path = require('path');
-const electronPath = path.join(__dirname, 'node_modules', '.bin', 'electron.cmd');
+const electronPath = path.join(__dirname, 'node_modules', '.bin', 'electron');
 
 module.exports = () => {
+  const extension = process.platform === 'win32' ? '.cmd' : '';
   return new Application({
-    path: electronPath,
-    args: [path.join('node_modules', 'ssf-desktop-api-electron', 'main.js')],
+    path: electronPath + extension,
+    args: [path.join('node_modules', 'containerjs-api-electron', 'main.js')],
     env: {
       TEST: true
     }
