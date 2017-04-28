e989444aa6c96054f7cb5db571c1c3b86d71fcf3
Ben Lambert <blambert@scottlogic.com>
2017-04-28 10:23:24 +0100

    Change to run all tests in api-tests

--- packages/api-openfin-demo/test/main.spec.js
+++
@@ -1,5 +0,0 @@
-const containerSetup = require('./test-container-setup');
-const containerjsTests = require('containerjs-api-tests');
-const mocha = require('mocha');
-
-containerjsTests(containerSetup, mocha);
---
+++ packages/api-tests/README.md
@@ -0,0 +1,8 @@
+# ContainerJS Testing
+This project contains the Spectron UI tests for the OpenFin and Electron APIs
+
+## Testing
+```
+npm i
+npm run test:ui
+```
