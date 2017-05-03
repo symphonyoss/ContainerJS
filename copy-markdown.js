bc0430efb8008b1f85c386a0816a66f1a307f042
Ben Lambert <blambert@scottlogic.com>
2017-05-03 10:09:47 +0100

    Change to copy api docs from docs folder

--- copy-markdown.js
+++ copy-markdown.js
@@ -11,7 +11,9 @@ directories.forEach((dir) => {
     if (path.extname(file) === '.md') {
       // Convert markdown to html
       const md = fs.readFileSync(path.join(srcDir, file), {encoding: 'utf8'});
-      const html = marked.parse(md);
+      // Need to strip out the file data used for gh-pages
+      const strippedMd = md.split('---')[2];
+      const html = marked.parse(strippedMd);
 
       // Append the html to the api demo file
       fs.appendFileSync(path.join(srcDir, file.substr(0, file.length - 3) + '.html'), html);
