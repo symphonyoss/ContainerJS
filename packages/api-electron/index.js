18bd27828334ae5551f0843f8fc6650e3dce71a3
Ben Lambert <blambert@scottlogic.com>
2017-05-04 15:13:50 +0100

    Remove now unused constants

--- packages/api-electron/index.js
+++ packages/api-electron/index.js
@@ -5,29 +5,13 @@ const {
   ipcMain: ipc
 } = require('electron');
 const path = require('path');
-const {
-  IpcModifiers,
-  IpcMessages
-} = require('./src/common/constants');
+const { IpcMessages } = require('./src/common/constants');
 
 let win;
 const windows = [];
 const preloadPath = path.join(__dirname, 'dist', 'ssf-desktop-api.js');
 
 module.exports = (appJson) => {
-  const eNotify = require('electron-notify');
-
-  ipc.on(IpcMessages.IPC_SSF_NOTIFICATION, (e, msg) => {
-    if (!msg.options) {
-      msg.options = {};
-    }
-
-    eNotify.notify({
-      title: msg.title,
-      text: msg.options.body
-    });
-  });
-
   ipc.on(IpcMessages.IPC_SSF_NEW_WINDOW, (e, msg) => {
     const options = Object.assign(
       {},
@@ -53,18 +37,9 @@ module.exports = (appJson) => {
       }
     });
 
-    e.returnValue = {
-      id: newWindow.id
-    };
+    e.returnValue = newWindow.id;
 
     windows.push(newWindow);
-  });
-
-  ipc.on(IpcMessages.IPC_SSF_CAPTURE_SCREEN_SNIPPET, (e) => {
-    e.sender.capturePage((image) => {
-      const dataUri = 'data:image/png;base64,' + image.toPng().toString('base64');
-      e.sender.send(IpcMessages.IPC_SSF_SCREEN_SNIPPET_CAPTURED, dataUri);
-    });
   });
 
   ipc.on(IpcMessages.IPC_SSF_SEND_MESSAGE, (e, msg) => {
@@ -113,225 +88,6 @@ const createInitialHiddenWindow = (appJson) => {
 
 const ready = (cb) => {
   app.on('ready', cb);
-};
-
-const sendSuccess = (sender, event, nonce, message) => {
-  sender.send(`${event}${IpcModifiers.SUCCESS}-${nonce}`, message);
-};
-
-const sendFailure = (sender, event, nonce, message) => {
-  sender.send(`${event}${IpcModifiers.ERROR}-${nonce}`, message);
-};
-
-ipc.on(IpcMessages.IPC_SSF_BLUR_WINDOW, (e, id, nonce) => {
-  const messageInfo = [e.sender, IpcMessages.IPC_SSF_BLUR_WINDOW, nonce];
-  callWindowFunction(id, messageInfo, win => win.blur());
-});
-
-ipc.on(IpcMessages.IPC_SSF_CLOSE_WINDOW, (e, id, nonce) => {
-  const messageInfo = [e.sender, IpcMessages.IPC_SSF_CLOSE_WINDOW, nonce];
-  callWindowFunction(id, messageInfo, win => win.close());
-});
-
-ipc.on(IpcMessages.IPC_SSF_FLASH_FRAME, (e, id, nonce, args) => {
-  const messageInfo = [e.sender, IpcMessages.IPC_SSF_FLASH_FRAME, nonce];
-  const [flag] = args;
-  callWindowFunction(id, messageInfo, win => win.flashFrame(flag));
-});
-
-ipc.on(IpcMessages.IPC_SSF_FOCUS_WINDOW, (e, id, nonce) => {
-  const messageInfo = [e.sender, IpcMessages.IPC_SSF_FOCUS_WINDOW, nonce];
-  callWindowFunction(id, messageInfo, win => win.focus());
-});
-
-ipc.on(IpcMessages.IPC_SSF_GET_WINDOW_BOUNDS, (e, id, nonce) => {
-  const messageInfo = [e.sender, IpcMessages.IPC_SSF_GET_WINDOW_BOUNDS, nonce];
-  callWindowFunction(id, messageInfo, win => win.getBounds());
-});
-
-ipc.on(IpcMessages.IPC_SSF_GET_WINDOW_ID, (e) => {
-  e.returnValue = BrowserWindow.fromWebContents(e.sender).id;
-});
-
-ipc.on(IpcMessages.IPC_SSF_GET_WINDOW_MAXIMUM_SIZE, (e, id, nonce) => {
-  const messageInfo = [e.sender, IpcMessages.IPC_SSF_GET_WINDOW_MAXIMUM_SIZE, nonce];
-  callWindowFunction(id, messageInfo, win => win.getMaximumSize());
-});
-
-ipc.on(IpcMessages.IPC_SSF_GET_WINDOW_MINIMUM_SIZE, (e, id, nonce) => {
-  const messageInfo = [e.sender, IpcMessages.IPC_SSF_GET_WINDOW_MINIMUM_SIZE, nonce];
-  callWindowFunction(id, messageInfo, win => win.getMinimumSize());
-});
-
-ipc.on(IpcMessages.IPC_SSF_GET_WINDOW_PARENT, (e, id, nonce) => {
-  const messageInfo = [e.sender, IpcMessages.IPC_SSF_GET_WINDOW_PARENT, nonce];
-  callWindowFunction(id, messageInfo, win => win.getParentWindow());
-});
-
-ipc.on(IpcMessages.IPC_SSF_GET_WINDOW_POSITION, (e, id, nonce) => {
-  const messageInfo = [e.sender, IpcMessages.IPC_SSF_GET_WINDOW_POSITION, nonce];
-  callWindowFunction(id, messageInfo, win => win.getPosition());
-});
-
-ipc.on(IpcMessages.IPC_SSF_GET_WINDOW_SIZE, (e, id, nonce) => {
-  const messageInfo = [e.sender, IpcMessages.IPC_SSF_GET_WINDOW_SIZE, nonce];
-  callWindowFunction(id, messageInfo, win => win.getSize());
-});
-
-ipc.on(IpcMessages.IPC_SSF_GET_WINDOW_TITLE, (e, id, nonce) => {
-  const messageInfo = [e.sender, IpcMessages.IPC_SSF_GET_WINDOW_TITLE, nonce];
-  callWindowFunction(id, messageInfo, win => win.getTitle());
-});
-
-ipc.on(IpcMessages.IPC_SSF_WINDOW_HAS_SHADOW, (e, id, nonce, args) => {
-  const messageInfo = [e.sender, IpcMessages.IPC_SSF_WINDOW_HAS_SHADOW, nonce];
-  callWindowFunction(id, messageInfo, win => win.hasShadow());
-});
-
-ipc.on(IpcMessages.IPC_SSF_HIDE_WINDOW, (e, id, nonce) => {
-  const messageInfo = [e.sender, IpcMessages.IPC_SSF_HIDE_WINDOW, nonce];
-  callWindowFunction(id, messageInfo, win => win.hide());
-});
-
-ipc.on(IpcMessages.IPC_SSF_IS_WINDOW_ALWAYS_ON_TOP, (e, id, nonce) => {
-  const messageInfo = [e.sender, IpcMessages.IPC_SSF_IS_WINDOW_ALWAYS_ON_TOP, nonce];
-  callWindowFunction(id, messageInfo, win => win.isAlwaysOnTop());
-});
-
-ipc.on(IpcMessages.IPC_SSF_IS_WINDOW_MAXIMIZABLE, (e, id, nonce) => {
-  const messageInfo = [e.sender, IpcMessages.IPC_SSF_IS_WINDOW_MAXIMIZABLE, nonce];
-  callWindowFunction(id, messageInfo, win => win.isMaximizable());
-});
-
-ipc.on(IpcMessages.IPC_SSF_IS_WINDOW_MAXIMIZED, (e, id, nonce) => {
-  const messageInfo = [e.sender, IpcMessages.IPC_SSF_IS_WINDOW_MAXIMIZED, nonce];
-  callWindowFunction(id, messageInfo, win => win.isMaximized());
-});
-
-ipc.on(IpcMessages.IPC_SSF_IS_WINDOW_MINIMIZABLE, (e, id, nonce) => {
-  const messageInfo = [e.sender, IpcMessages.IPC_SSF_IS_WINDOW_MINIMIZABLE, nonce];
-  callWindowFunction(id, messageInfo, win => win.isMinimizable());
-});
-
-ipc.on(IpcMessages.IPC_SSF_IS_WINDOW_MINIMIZED, (e, id, nonce) => {
-  const messageInfo = [e.sender, IpcMessages.IPC_SSF_IS_WINDOW_MINIMIZED, nonce];
-  callWindowFunction(id, messageInfo, win => win.isMinimized());
-});
-
-ipc.on(IpcMessages.IPC_SSF_IS_WINDOW_RESIZABLE, (e, id, nonce) => {
-  const messageInfo = [e.sender, IpcMessages.IPC_SSF_IS_WINDOW_RESIZABLE, nonce];
-  callWindowFunction(id, messageInfo, win => win.isResizable());
-});
-
-ipc.on(IpcMessages.IPC_SSF_LOAD_URL, (e, id, nonce, args) => {
-  const messageInfo = [e.sender, IpcMessages.IPC_SSF_LOAD_URL, nonce];
-  const [url] = args;
-  callWindowFunction(id, messageInfo, win => win.loadURL(url));
-});
-
-ipc.on(IpcMessages.IPC_SSF_RELOAD_WINDOW, (e, id, nonce) => {
-  const messageInfo = [e.sender, IpcMessages.IPC_SSF_RELOAD_WINDOW, nonce];
-  callWindowFunction(id, messageInfo, win => win.reload());
-});
-
-ipc.on(IpcMessages.IPC_SSF_RESTORE_WINDOW, (e, id, nonce) => {
-  const messageInfo = [e.sender, IpcMessages.IPC_SSF_RESTORE_WINDOW, nonce];
-  callWindowFunction(id, messageInfo, win => win.restore());
-});
-
-ipc.on(IpcMessages.IPC_SSF_SET_WINDOW_ALWAYS_ON_TOP, (e, id, nonce, args) => {
-  const messageInfo = [e.sender, IpcMessages.IPC_SSF_SET_WINDOW_ALWAYS_ON_TOP, nonce];
-  const [alwaysOnTop] = args;
-  callWindowFunction(id, messageInfo, win => win.setAlwaysOnTop(alwaysOnTop));
-});
-
-ipc.on(IpcMessages.IPC_SSF_SET_WINDOW_BOUNDS, (e, id, nonce, args) => {
-  const messageInfo = [e.sender, IpcMessages.IPC_SSF_SET_WINDOW_BOUNDS, nonce];
-  const [bounds] = args;
-  callWindowFunction(id, messageInfo, win => win.setBounds(bounds));
-});
-
-ipc.on(IpcMessages.IPC_SSF_SET_WINDOW_ICON, (e, id, nonce, args) => {
-  const messageInfo = [e.sender, IpcMessages.IPC_SSF_SET_WINDOW_BOUNDS, nonce];
-  const [icon] = args;
-  callWindowFunction(id, messageInfo, win => win.setIcon(icon));
-});
-
-ipc.on(IpcMessages.IPC_SSF_SET_WINDOW_MAXIMIZABLE, (e, id, nonce, args) => {
-  const messageInfo = [e.sender, IpcMessages.IPC_SSF_SET_WINDOW_MAXIMIZABLE, nonce];
-  const [maximizable] = args;
-  callWindowFunction(id, messageInfo, win => win.setMaximizable(maximizable));
-});
-
-ipc.on(IpcMessages.IPC_SSF_SET_WINDOW_MAXIMUM_SIZE, (e, id, nonce, args) => {
-  const messageInfo = [e.sender, IpcMessages.IPC_SSF_SET_WINDOW_MAXIMUM_SIZE, nonce];
-  const [width, height] = args;
-  callWindowFunction(id, messageInfo, win => win.setMaximumSize(width, height));
-});
-
-ipc.on(IpcMessages.IPC_SSF_SET_WINDOW_MINIMIZABLE, (e, id, nonce, args) => {
-  const messageInfo = [e.sender, IpcMessages.IPC_SSF_SET_WINDOW_MINIMIZABLE, nonce];
-  const [minimizable] = args;
-  callWindowFunction(id, messageInfo, win => win.setMinimizable(minimizable));
-});
-
-ipc.on(IpcMessages.IPC_SSF_SET_WINDOW_MINIMUM_SIZE, (e, id, nonce, args) => {
-  const messageInfo = [e.sender, IpcMessages.IPC_SSF_SET_WINDOW_MINIMUM_SIZE, nonce];
-  const [width, height] = args;
-  callWindowFunction(id, messageInfo, win => win.setMinimumSize(width, height));
-});
-
-ipc.on(IpcMessages.IPC_SSF_SET_WINDOW_POSITION, (e, id, nonce, args) => {
-  const messageInfo = [e.sender, IpcMessages.IPC_SSF_SET_WINDOW_POSITION, nonce];
-  const [x, y] = args;
-  callWindowFunction(id, messageInfo, win => win.setPosition(x, y));
-});
-
-ipc.on(IpcMessages.IPC_SSF_SET_WINDOW_RESIZABLE, (e, id, nonce, args) => {
-  const messageInfo = [e.sender, IpcMessages.IPC_SSF_SET_WINDOW_RESIZABLE, nonce];
-  const [resizable] = args;
-  callWindowFunction(id, messageInfo, win => win.setResizable(resizable));
-});
-
-ipc.on(IpcMessages.IPC_SSF_SET_WINDOW_SIZE, (e, id, nonce, args) => {
-  const messageInfo = [e.sender, IpcMessages.IPC_SSF_SET_WINDOW_SIZE, nonce];
-  const [width, height] = args;
-  callWindowFunction(id, messageInfo, win => win.setSize(width, height));
-});
-
-ipc.on(IpcMessages.IPC_SSF_SET_WINDOW_SKIP_TASKBAR, (e, id, nonce, args) => {
-  const messageInfo = [e.sender, IpcMessages.IPC_SSF_SET_WINDOW_SKIP_TASKBAR, nonce];
-  const [skipTaskbar] = args;
-  callWindowFunction(id, messageInfo, win => win.setSkipTaskbar(skipTaskbar));
-});
-
-ipc.on(IpcMessages.IPC_SSF_SHOW_WINDOW, (e, id, nonce) => {
-  const messageInfo = [e.sender, IpcMessages.IPC_SSF_SHOW_WINDOW, nonce];
-  callWindowFunction(id, messageInfo, win => win.show());
-});
-
-ipc.on(IpcMessages.IPC_SSF_MAXIMIZE_WINDOW, (e, id, nonce) => {
-  const messageInfo = [e.sender, IpcMessages.IPC_SSF_MAXIMIZE_WINDOW, nonce];
-  callWindowFunction(id, messageInfo, win => win.maximize());
-});
-
-ipc.on(IpcMessages.IPC_SSF_MINIMIZE_WINDOW, (e, id, nonce) => {
-  const messageInfo = [e.sender, IpcMessages.IPC_SSF_MINIMIZE_WINDOW, nonce];
-  callWindowFunction(id, messageInfo, win => win.minimize());
-});
-
-ipc.on(IpcMessages.IPC_SSF_UNMAXIMIZE_WINDOW, (e, id, nonce) => {
-  const messageInfo = [e.sender, IpcMessages.IPC_SSF_UNMAXIMIZE_WINDOW, nonce];
-  callWindowFunction(id, messageInfo, win => win.unmaximize());
-});
-
-const callWindowFunction = (id, messageInfo, windowFunction) => {
-  getWindowFromId(id, (win) => {
-    const returnValue = windowFunction(win);
-    sendSuccess(...messageInfo, returnValue);
-  }, (error) => {
-    sendFailure(...messageInfo, error);
-  });
 };
 
 const getWindowFromId = (id, cb, errorcb) => {
