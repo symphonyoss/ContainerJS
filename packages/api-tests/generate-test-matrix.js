44e0c7168022185d28a3181c7f28ea2f98f10198
Ben Lambert <blambert@scottlogic.com>
2017-05-15 11:05:20 +0100

    Add test coverage matrix (#113)

---
+++ packages/api-tests/generate-test-matrix.js
@@ -0,0 +1,105 @@
+const fs = require('fs');
+const path = require('path');
+const electronTestOutput = JSON.parse(fs.readFileSync(path.join(process.cwd(), 'coverage', 'electron.json')));
+const openfinTestOutput = JSON.parse(fs.readFileSync(path.join(process.cwd(), 'coverage', 'openfin.json')));
+
+const testTable = {
+  electron: {},
+  openfin: {}
+};
+
+// Read as 10% or less, 25% or less etc
+const percentageColors = {
+  10: '#f45858',
+  25: '#f2f23c',
+  50: '#aeea3f',
+  75: '#85ea4f',
+  100: '#50ce5b'
+};
+
+const testStatus = {
+  pass: 'pass',
+  skip: 'pending',
+  error: 'error'
+};
+
+const getColorCode = (percent) => {
+  let colorCode = '';
+  Object.keys(percentageColors).some((key) => {
+    if (percent <= parseInt(key)) {
+      colorCode = percentageColors[key];
+      return true;
+    }
+    return false;
+  });
+  return colorCode;
+};
+
+const listOfTests = [];
+const tagCount = {};
+// generate the total test count
+const generateTotals = (testOutput) => {
+  testOutput.forEach((test) => {
+    if (!listOfTests.find((t) => t === test.title)) {
+      listOfTests.push(test.title);
+      test.tags.forEach((tag) => {
+        if (test.status !== testStatus.skip) {
+          if (tagCount[tag]) {
+            tagCount[tag]++;
+          } else {
+            tagCount[tag] = 1;
+          }
+        } else if (!tagCount[tag]) {
+          tagCount[tag] = 0;
+        }
+      });
+    }
+  });
+};
+
+const passCount = (table, tag, test) => {
+  if (!table[tag]) {
+    table[tag] = 0;
+  }
+
+  if (test.status === testStatus.pass) {
+    table[tag]++;
+  }
+};
+
+electronTestOutput.forEach((test) => {
+  test.tags.forEach((tag) => passCount(testTable.electron, tag, test));
+});
+
+openfinTestOutput.forEach((test) => {
+  test.tags.forEach((tag) => passCount(testTable.openfin, tag, test));
+});
+
+generateTotals(electronTestOutput);
+generateTotals(openfinTestOutput);
+
+const sortedTags = Object.keys(tagCount).sort();
+
+let markdownString = '| Method | Electron | OpenFin |\n|:---|:---:|:---:|\n';
+
+sortedTags.forEach((tag) => {
+  const electronPassed = testTable.electron[tag];
+  const openfinPassed = testTable.openfin[tag];
+  const total = tagCount[tag];
+  const electronColor = total > 0 ? getColorCode((electronPassed / total) * 100) : '';
+  const openfinColor = total > 0 ? getColorCode((openfinPassed / total) * 100) : '';
+  const label = tag.substring(1); // Removes the # from the front of the tag
+  markdownString += `|${label}|<div style="background-color:${electronColor}">${electronPassed}/${total}</div>|<div style="background-color:${openfinColor}">${openfinPassed}/${total}</div>|\n`;
+});
+
+const ghPagesMarkdown =
+`---
+id: testMatrix
+title: Test Report
+permalink: docs/test-matrix.html
+layout: docs
+sectionid: docs
+---\n
+`;
+
+fs.writeFileSync(path.join(__dirname, '..', '..', 'docs', 'docs', 'test-matrix.md'), ghPagesMarkdown + markdownString);
---
+++ packages/api-tests/reporter.js
@@ -0,0 +1,68 @@
+var mocha = require('mocha');
+var fs = require('fs');
+var path = require('path');
+
+module.exports = JsonReporter;
+const testContainer = process.env.MOCHA_CONTAINER;
+
+let jsonOutput = [];
+// Regex: (# followed by at least 1 word character, dot or bracket followed by 1 space) one or more times
+// Example: #ssf.Window.hide #electron
+const testTagPattern = /(#[\w.()]+[\s]?)+/;
+
+function JsonReporter(runner) {
+  mocha.reporters.Base.call(this, runner);
+  var passes = 0;
+  var pending = 0;
+  var failures = 0;
+
+  runner.on('suite', function(suite) {
+    console.log(suite.title);
+  });
+
+  runner.on('pass', function(test) {
+    passes++;
+    const tags = test.title.match(testTagPattern);
+    jsonOutput.push({
+      title: test.fullTitle(),
+      status: 'pass',
+      container: testContainer,
+      tags: tags === null ? [] : tags[0].split(' ')
+    });
+    console.log('%s %s', mocha.reporters.Base.symbols.ok, test.title);
+  });
+
+  // Runs when a test has been skipped
+  runner.on('pending', function(test) {
+    pending++;
+    const tags = test.title.match(testTagPattern);
+    jsonOutput.push({
+      title: test.fullTitle(),
+      status: 'pending',
+      container: testContainer,
+      tags: tags === null ? [] : tags[0].split(' ')
+    });
+    console.log('%s %s', '-', test.title);
+  });
+
+  runner.on('fail', function(test, err) {
+    failures++;
+    const tags = test.title.match(testTagPattern);
+    jsonOutput.push({
+      title: test.fullTitle(),
+      status: 'fail',
+      container: testContainer,
+      tags: tags === null ? [] : tags[0].split(' ')
+    });
+    console.log('%s %s -- error: %s', mocha.reporters.Base.symbols.err, test.title, err.message);
+  });
+
+  runner.on('end', function() {
+    console.log('pass:%d pending:%d failure:%d', passes, pending, failures);
+    if (!fs.existsSync(path.join(process.cwd(), 'coverage'))) {
+      fs.mkdirSync(path.join(process.cwd(), 'coverage'));
+    }
+    fs.writeFileSync(path.join(process.cwd(), 'coverage', testContainer + '.json'), JSON.stringify(jsonOutput));
+    process.exit(failures > 0 ? 1 : 0);
+  });
+}
