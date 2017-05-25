276d4e06cbe3f19ab01926d1ee1e835769e86a80
Colin Eberhardt <colin.eberhardt@gmail.com>
2017-05-25 09:23:05 +0100

    removed explicit reference to interface file
    ... the fact that it is referenced in tsconfig should be enough

--- packages/api-browser/preload.ts
+++ packages/api-browser/preload.ts
@@ -2,7 +2,6 @@ import app from './src/app';
 import MessageService from './src/message-service';
 import ScreenSnippet from './src/screen-snippet';
 import Window from './src/window';
-import './src/interface';
 
 export {
   app,
--- packages/api-electron/preload.ts
+++ packages/api-electron/preload.ts
@@ -3,7 +3,6 @@ import app from './src/preload/app';
 import MessageService from './src/preload/message-service';
 import ScreenSnippet from './src/preload/screen-snippet';
 import Window from './src/preload/window';
-import './src/interface';
 
 export {
   app,
