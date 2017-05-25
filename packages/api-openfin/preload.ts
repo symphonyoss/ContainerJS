dfe08c191785fe67542d011b4f66dafd9758f75d
Colin Eberhardt <colin.eberhardt@gmail.com>
2017-05-25 09:38:21 +0100

    remove the interface copy step (#160)
    * remove the interface copy step
    
    * removed explicit reference to interface file
    
    ... the fact that it is referenced in tsconfig should be enough

--- packages/api-openfin/preload.ts
+++ packages/api-openfin/preload.ts
@@ -3,7 +3,6 @@ import app from './src/app';
 import MessageService from './src/message-service';
 import ScreenSnippet from './src/screen-snippet';
 import Window from './src/window';
-import './src/interface';
 
 export {
   app,
