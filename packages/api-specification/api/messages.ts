618fff7ffceca1f98290f797d853c736357bbb28
Kiril Popov <kiril.popov@tick42.com>
2017-05-26 18:17:22 +0300

    comment

--- packages/api-specification/api/messages.ts
+++ packages/api-specification/api/messages.ts
@@ -1,4 +1,8 @@
-// Revise - are we ok with bus pub/sub (the current model) or we want RPC
+// &#9888; TODO Revise - are we ok with bus pub/sub (the current model) or we want to propose AGM like RPC
+
+/**
+ * Currently this is based on OpenFin bus
+ */
 export interface MessagesAPI{
     send(windowId: string, topic: string, message: string|object): void;
     subscribe(windowId: string, topic: string, listener: Function): void;
