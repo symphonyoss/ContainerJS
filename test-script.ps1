b8333f5127cc2a2d832a38cd7417ee4c378a1158
Ben Lambert <blambert@scottlogic.com>
2017-05-26 13:45:18 +0100

    test

--- test-script.ps1
+++ test-script.ps1
@@ -2,6 +2,8 @@ node --version
 npm --version
 npm run test
 
+Write-Host "PR NUMBER: $($env:APPVEYOR_PULL_REQUEST_NUMBER)"
+
 # Don't run tests on PRs
 IF ($env:APPVEYOR_REPO_BRANCH -eq "master" -And $env:APPVEYOR_PULL_REQUEST_NUMBER -eq "") {
   npm run test:ci
