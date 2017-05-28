79ce8b60c341fcf08a062a62cd8a01a9a67503bc
Kiril Popov <kiril.popov@tick42.com>
2017-05-28 23:52:43 +0300

    Updates

--- packages/api-specification/api/application.ts
+++ packages/api-specification/api/application.ts
@@ -1,14 +1,28 @@
 export interface ApplicationAPI {
+    /**
+     * Returns an array containing the command line arguments passed when application was launched
+     */
+    argv: string[];
+
+    /**
+     * Application configuration object (as defined in app.json file)
+     */
     config: ApplicationConfig;
 
-    /** Sets the counter badge for current app. Setting the count to 0 will hide the badge.*/
-    setBadgeCount(count: number);
-
-    /** The current value displayed in the counter badge.*/
+    /**
+     * The current value displayed in the counter badge.
+     */
     getBadgeCount(): number;
 
-    /** Returns an array containing the command line arguments passed when application was launched */
-    argv: string[];
+    /**
+     * Sets the counter badge for current app. Setting the count to 0 will hide the badge.
+     */
+    setBadgeCount(count: number);
+
+    /**
+     * Shutdown the application
+     */
+    shutdown: () => void;
 }
 
 export interface ApplicationConfig {
