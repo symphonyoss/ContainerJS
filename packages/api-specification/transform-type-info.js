f6e2f72596c3e3f72e62fb8a286e4d8d81388ea7
Ben Lambert <blambert@scottlogic.com>
2017-06-08 11:09:00 +0100

    Add styling to documentation pages (#181)

--- packages/api-specification/transform-type-info.js
+++ packages/api-specification/transform-type-info.js
@@ -18,8 +18,9 @@ console.log(outpath);
 const flatten = (arr) => arr.reduce((a, b) => a.concat(b), []);
 
 // html elements are a bit complex, this simplifies their construction
-const element = (tag, child) => ({
+const element = (tag, child, attr) => ({
   node: 'element',
+  attr,
   tag,
   // if child is not an array, turn it into one - also, filter out any undefined children
   child: (Array.isArray(child) ? child : [child]).filter(d => d)
@@ -31,20 +32,25 @@ const text = (text) => ({
   text
 });
 
+const colorBoundaries = [0, 10, 25, 50, 75, 100];
+
+const testResultPercentage = (passed, total) => total > 0 ? `test-color-${colorBoundaries.filter((c) => c <= Math.round((passed / total) * 100)).slice(-1).pop()}` : '';
+
 // there are a number of elements that just host a single text node, this
 // simplifies their construction
-const textElement = (tag) => (body) => element(tag, text(body));
+const textElement = (tag) => (body, attr) => element(tag, text(body), attr);
+const h5 = textElement('h5');
 const h4 = textElement('h4');
-const h1 = textElement('h1');
 const h3 = textElement('h3');
 const h2 = textElement('h2');
 const p = textElement('p');
 const dt = textElement('dt');
 const dd = textElement('dd');
+const span = textElement('span');
 
 // there are a number of elements that host multiple children, this
 // simplifies their construction
-const parentElement = (tag) => (children) => element(tag, children);
+const parentElement = (tag) => (children, attr) => element(tag, children, attr);
 const section = parentElement('section');
 const dl = parentElement('dl');
 
@@ -59,7 +65,7 @@ if (program.testfile) {
       const [, className, methodName] = testMethod.split('.');
       const method = jspath.apply(`..{.kindString === "Class" && .name === "${className}"}.children{.kindString === "Method" && .name === "${methodName}"}`, typeInfo);
       if (method.length > 0) {
-        method[0].results = results;
+        method[0].signatures[0].results = results;
       } else {
         console.warn(`unable to find the method ${testMethod} within the typescript documentation`);
       }
@@ -81,6 +87,13 @@ const formatType = (type) => {
   return 'UNKNOWN';
 };
 
+const testResults = (title, results) => {
+  return [
+    h5(title, {class: 'test-result-title'}),
+    span(`${results.passed}/${results.total}`, {class: 'test-result ' + testResultPercentage(results.passed, results.total)})
+  ];
+};
+
 const documentClass = (className) => {
   const rules = [
     // perform a 'deep' match to find any class declarations, then recursively
@@ -94,49 +107,52 @@ const documentClass = (className) => {
     jsont.pathRule(
       '.{.kindString === "Class"}', d =>
         section([
-          h1(d.match.name),
+          h2(d.match.name),
           // create the various sections by filtering the children based on their 'kind'
-          h2('Constructors'),
-          section(d.runner(jspath.apply('.{.kindString === "Constructor"}', d.match.children))),
-          h2('Properties'),
-          section(d.runner(jspath.apply('.{.kindString === "Property"}', d.match.children))),
-          h2('Methods'),
-          section(d.runner(jspath.apply('.{.kindString === "Method"}', d.match.children)))
-        ])
+          jspath.apply('.{.kindString === "Constructor"}', d.match.children).length !== 0 ? h3('Constructors') : undefined,
+          jspath.apply('.{.kindString === "Constructor"}', d.match.children).length !== 0 ? section(d.runner(jspath.apply('.{.kindString === "Constructor"}', d.match.children)), {class: 'constructors'}) : undefined,
+          jspath.apply('.{.kindString === "Property"}', d.match.children).length !== 0 ? h3('Properties') : undefined,
+          jspath.apply('.{.kindString === "Property"}', d.match.children).length !== 0 ? section(d.runner(jspath.apply('.{.kindString === "Property"}', d.match.children)), {class: 'properties'}) : undefined,
+          jspath.apply('.{.kindString === "Method"}', d.match.children).length !== 0 ? h3('Methods') : undefined,
+          jspath.apply('.{.kindString === "Method"}', d.match.children).length !== 0 ? section(d.runner(jspath.apply('.{.kindString === "Method"}', d.match.children)), {class: 'methods'}) : undefined
+        ], {id: className, class: 'docs-title'})
     ),
     jsont.pathRule(
       '.{.kindString === "Method" || .kindString === "Constructor"}', d =>
         // we take just the first signature as the definition of the method, not
         // sure when there might be multiple signatures?!
         section([
-          d.runner(d.match.signatures[0]),
-          h4('test results'),
-          p(JSON.stringify(d.match.results))
-        ])
+          d.runner(d.match.signatures[0])
+        ], {class: 'method'})
     ),
     jsont.pathRule(
       '.{.kindString === "Property"}', d =>
         section([
-          h4(d.match.name),
+          h5(d.match.name, {class: 'code'}),
           p(d.match.comment ? d.match.comment.shortText : '')
-        ])
+        ], {class: 'property', id: d.match.name})
     ),
     jsont.pathRule(
       '.{.kindString === "Call signature" || .kindString === "Constructor signature"}', d =>
         section([
-          h3(d.match.name),
+          h4(d.match.name, {class: 'method-name', id: `${className}-${d.match.name}`}),
+          d.match.results ? section([
+            ...testResults('Electron', d.match.results.electron),
+            ...testResults('OpenFin', d.match.results.openfin),
+            ...testResults('Browser', d.match.results.browser)
+          ], {class: 'test-results'}) : undefined,
           p(d.match.comment ? d.match.comment.shortText : ''),
-          h4('Returns'),
-          p(formatType(d.match.type)),
-          d.match.parameters ? h4('Arguments') : undefined,
+          d.match.parameters ? h5('Arguments') : undefined,
           // NOTE: we flatten because each parameter returns an array of dt / dd, we want to
           // flatten from [[dt, dd], [dt, dd]] to [dt, dd, dt, dd]
-          d.match.parameters ? dl(flatten(d.runner(d.match.parameters))) : undefined
+          d.match.parameters ? dl(flatten(d.runner(d.match.parameters))) : undefined,
+          p('Returns:', {class: 'return-text'}),
+          p(formatType(d.match.type), {class: 'code return-type'})
         ])
     ),
     jsont.pathRule(
       '.{.kindString === "Parameter"}', d => [
-        dt(`${d.match.name} [${formatType(d.match.type)}]`),
+        dt(`${d.match.name} [${formatType(d.match.type)}]`, {class: 'code argument'}),
         dd(d.match.comment ? d.match.comment.text : '')
       ]
     )
@@ -144,22 +160,24 @@ const documentClass = (className) => {
 
   const json = jsont.transform(typeInfo, rules);
   const html = json2html(json);
-  const yml = matter.stringify(html, {
-    layout: 'api',
-    sectionid: 'docs',
-    class: className
-  });
 
   if (!fs.existsSync(outpath)) {
     fs.mkdirSync(outpath);
   }
-  fs.writeFileSync(`${outpath}/${className}.html`, yml);
+  fs.appendFileSync(`${outpath}/Docs.html`, html);
 };
 
-// find the list of classes - these are generated as separate documentation pages
+// find the list of classes
 const classes = jsont.transform(typeInfo, [
   jsont.pathRule('..children{.kindString === "Class"}', d => d.runner()),
   jsont.pathRule('.{.kindString === "Class"}', d => d.match.name)
 ]);
 
+const yml = matter.stringify('', {
+  layout: 'api',
+  sectionid: 'docs',
+  class: 'docs'
+});
+
+fs.writeFileSync(`${outpath}/Docs.html`, yml);
 classes.forEach(documentClass);
