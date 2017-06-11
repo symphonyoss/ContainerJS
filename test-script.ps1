1b120ebcea4faa157e5d55f9f2217959318ecc86
James Leftley <me@tfel.net>
2017-06-11 13:25:25 +0100

    Update scripts across packages for consistency

--- test-script.ps1
+++ test-script.ps1
@@ -1,7 +1,3 @@
-node --version
-npm --version
-npm run test
-
 # Don't run tests on PRs
 IF ($env:APPVEYOR_REPO_BRANCH -eq "master" -And (-Not (Test-Path Env:\APPVEYOR_PULL_REQUEST_NUMBER))) {
   npm run test:ci
