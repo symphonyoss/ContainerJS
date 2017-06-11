1e30d261a9a567727657c2c1a5cc4892b68cf05b
Ben Lambert <blambert@scottlogic.com>
2017-06-11 08:44:50 +0100

    Fix documentation tab in docs site (#187)

---
+++ js/main.js
@@ -0,0 +1,19 @@
+(function() {
+  if (window.location.pathname.split('/').pop().toLowerCase() === 'docs') {
+    const menu = document.getElementById('docs-menu');
+    const sections = document.getElementsByClassName('docs-title');
+    for (let i = 0; i < sections.length; i++) {
+      const name = sections[i].id;
+      const li = document.createElement('li');
+      li.setAttribute('class', 'list-item');
+
+      const link = document.createElement('a');
+      link.setAttribute('class', 'nav-link');
+      link.setAttribute('href', '#' + name);
+      link.text = name;
+
+      li.appendChild(link);
+      menu.appendChild(li);
+    }
+  }
+})();
