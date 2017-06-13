e009a8a052e70a8dcac8e72b91b9463aab2e37d6
Ben Lambert <blambert@scottlogic.com>
2017-06-13 12:01:18 +0100

    Add links to symphony API docs

--- packages/api-symphony-compatibility/src/mapping.ts
+++ packages/api-symphony-compatibility/src/mapping.ts
@@ -2,26 +2,31 @@ const containerjsSsf = ssf;
 
 namespace map {
   export class ssf {
+    /** API defined at https://symphonyoss.atlassian.net/wiki/display/WGDWAPI/Activate+API */
     static activate(windowName: string) {
       // Could use window.focus in containerjs, but no way of selecting window by name yet
       return;
     }
 
+    /** API defined at https://symphonyoss.atlassian.net/wiki/display/WGDWAPI/Activity+API */
     static registerActivityDetection(throttle: number, callback: Function) {
       // Not implemented in containerjs
       return;
     }
 
+    /** API defined at https://symphonyoss.atlassian.net/wiki/display/WGDWAPI/BadgeCount+API */
     static setBadgeCount(Number: number) {
       // Not implemented in containerjs
       return;
     }
 
+    /** API defined at https://symphonyoss.atlassian.net/wiki/display/WGDWAPI/getMediaSources+API */
     static getMediaSources(options: any, callback: Function) {
       // Not implemented in containerjs
       return;
     }
 
+    /** API defined at https://symphonyoss.atlassian.net/wiki/display/WGDWAPI/RegisterBoundsChange+API */
     static registerBoundsChange(callback: Function) {
       // Not fully implemented in containerjs, only listens to the current window, not its children as well
       containerjsSsf.Window.getCurrentWindow().addListener('resize', () => {
@@ -36,6 +41,7 @@ namespace map {
       });
     }
 
+    /** API defined at https://symphonyoss.atlassian.net/wiki/display/WGDWAPI/Version+API */
     static getVersionInfo() {
       // Not implemented in containerjs
       return new Promise((resolve, reject) => {
@@ -45,6 +51,7 @@ namespace map {
   }
 
   export namespace ssf {
+    /** API defined at https://symphonyoss.atlassian.net/wiki/display/WGDWAPI/ScreenSnippet+API */
     export class ScreenSnippet {
       capture() {
         return new containerjsSsf.ScreenSnippet().capture();
