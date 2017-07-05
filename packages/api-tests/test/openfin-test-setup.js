5bd97162ccb936f04153e22481ba908032910f4b
Ben Lambert <blambert@scottlogic.com>
2017-07-05 14:52:12 +0100

    Add cli wrapper around OpenFin (#228)

--- packages/api-tests/test/openfin-test-setup.js
+++ packages/api-tests/test/openfin-test-setup.js
@@ -3,7 +3,7 @@ const spawn = require('child_process').spawn;
 
 module.exports = (timeout) => {
   if (process.platform === 'win32') {
-    const args = ['/c', 'openfin -l -c http://localhost:5000/app.json'];
+    const args = ['/c', 'ssf-openfin -c app.json -o ./src/openfinapp.json -v 6.49.20.22 -u http://localhost:5000/openfinapp.json'];
     spawn('cmd.exe', args);
   } else {
     const args = ['-c', 'openfin -l -c http://localhost:5000/app.json'];
