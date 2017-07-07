6b691adf4c9e51dec67093fa5621caef079f7ee5
Ben Lambert <blambert@scottlogic.com>
2017-07-07 11:24:45 +0100

    Move event emitter into its own file

--- packages/api-specification/interface/window-extension.ts
+++
@@ -1,8 +0,0 @@
-declare interface Window {
-  /**
-   * Create a notification
-   * @param title - The title text of the notification.
-   * @param options - The notification options.
-   */
-  Notification(title: string, options: ssf.NotificationOptions): void;
-}
