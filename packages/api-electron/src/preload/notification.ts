32e5f7caa4bdbd3008d7344b28bae82a7773a2f6
Andy Lee <alee@scottlogic.co.uk>
2017-07-07 13:36:21 +0100

    Added icon and image support for Notification
    I've tried to get the display as similar as possible between the
    three containers, but there are some differences - browser
    doesn't let you control the image size (and it's big), while
    OpenFin doesn't seem to have a way to alter the height.

--- packages/api-electron/src/preload/notification.ts
+++ packages/api-electron/src/preload/notification.ts
@@ -2,16 +2,45 @@ const remote = require('electron').remote;
 const eNotify = remote.require('electron-notify');
 const PERMISSION_GRANTED: ssf.NotificationPermission = 'granted';
 
+// Image style moves the image below the icon and title/body,
+// to match the style of native notifications
+const imageStyle = {
+        overflow: 'hidden',
+        display: 'block',
+        position: 'absolute',
+        bottom: 10
+      };
+const HEIGHT_WITHOUT_IMAGE = 65;
+const HEIGHT_WITH_IMAGE = 175;
+
 class Notification implements ssf.Notification {
   constructor(title: string, options: ssf.NotificationOptions) {
     if (!options) {
       options = {};
     }
 
+    eNotify.setConfig({
+      appIcon: this.getAbsoluteUrl(options.icon),
+      height: options.image ? HEIGHT_WITH_IMAGE : HEIGHT_WITHOUT_IMAGE,
+      defaultStyleImage: imageStyle
+    });
+
     eNotify.notify({
       title: title,
-      text: options.body
+      text: options.body,
+      image: this.getAbsoluteUrl(options.image)
     });
+  }
+
+  getAbsoluteUrl(url) {
+    if (url && !url.startsWith('http:') && !url.startsWith('http:')) {
+      const path = url.startsWith('/')
+          ? location.origin
+          : location.href.substring(0, location.href.lastIndexOf('/'));
+
+      return `${path}/${url}`
+    }
+    return url
   }
 
   static requestPermission(): Promise<ssf.NotificationPermission> {
