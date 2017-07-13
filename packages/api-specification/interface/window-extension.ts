25e84a4e0fd65ea1b23c25c926281436cb49f7c3
Ben Lambert <blambert@scottlogic.com>
2017-07-13 16:01:31 +0100

    Add comment about window interface extension

--- packages/api-specification/interface/window-extension.ts
+++ packages/api-specification/interface/window-extension.ts
@@ -1,3 +1,10 @@
+/*
+ * Both of these are needed for screen orientation in OpenFin and Browser.
+ * This is because screen orientation is a non standard feature, even though
+ * it is implemented in Chrome and FireFox browsers, so there are no typings.
+ * This gives us the typings, although a very stripped down version
+ */
+
 /**
  * @ignore
  */
