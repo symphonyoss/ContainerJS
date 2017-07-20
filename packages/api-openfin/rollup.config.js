34d8aeb4da0b32e9eb1f9f161dd289316d23e2fb
Ben Lambert <blambert@scottlogic.com>
2017-07-20 14:55:28 +0100

    Fix OpenFin preload and remove api-bundle (#276)
    * Remove preload from app.json
    
    * Remove api-bundle
    
    * Update demos and docs
    
    * Add ssf check to typescript
    
    * Fix capitalisation
    
    * Use unpkg url in docs
    
    * Remove comments

--- packages/api-openfin/rollup.config.js
+++ packages/api-openfin/rollup.config.js
@@ -1,6 +1,14 @@
+import resolve from 'rollup-plugin-node-resolve';
+
 export default {
   entry: 'build/es/index.js',
   format: 'umd',
   moduleName: 'ssf',
-  dest: 'build/dist/containerjs-api.js'
+  dest: 'build/dist/containerjs-api.js',
+  plugins: [
+    resolve({
+      jsnext: true,
+      main: true
+    })
+  ]
 };
