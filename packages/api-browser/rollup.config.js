77d4f905138b77b2cf20461dffa4d7b7d81aff68
Ben Lambert <blambert@scottlogic.com>
2017-07-20 14:38:56 +0100

    Remove comments

--- packages/api-browser/rollup.config.js
+++ packages/api-browser/rollup.config.js
@@ -1,24 +1,3 @@
-// import resolve from 'rollup-plugin-node-resolve';
-// import commonjs from 'rollup-plugin-commonjs';
-
-// export default {
-//   entry: 'bundle.js',
-//   moduleName: 'ssf',
-//   format: 'umd',
-//   plugins: [
-//     resolve({
-//       module: true,
-//       main: true,
-//       browser: false,
-//       jsnext: true
-//     }),
-//     commonjs({
-//       include: '**'
-//     })
-//   ],
-//   dest: 'build/dist/containerjs-api.js'
-// };
-
 import resolve from 'rollup-plugin-node-resolve';
 
 export default {
