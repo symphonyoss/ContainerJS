3a595b9a8dff96e6204505f126f4f906389f161c
Andy Lee <alee@scottlogic.co.uk>
2017-07-25 14:51:54 +0100

    Can send message to "*" (all windows) in OpenFin and Electron (#297)
    * Can send message to "*" (all windows) in OpenFin and Electron
    
    Browser implementation coming next
    Electron needs to send the message out to all Exectron windows if windowId is *
    OpenFin implemented "*" already but didn't exclude receiving the message in the same window
    
    * tslint

--- packages/api-openfin/src/message-service.ts
+++ packages/api-openfin/src/message-service.ts
@@ -1,3 +1,5 @@
+const listenerMap = new Map();
+
 export class MessageService implements ssf.MessageService {
   // Window ID should be in the form 'application-uuid:window-name'
   static send(windowId: string, topic: string, message: any) {
@@ -11,22 +13,53 @@ export class MessageService implements ssf.MessageService {
   }
 
   static subscribe(windowId: string, topic: string, listener: (...args: any[]) => void) {
+    const thisWindowId = fin.desktop.Window.getCurrent().uuid;
+    const receiveMessage = (message, senderId) => {
+      // Don't send to self
+      if (senderId !== thisWindowId) {
+        listener(message, senderId);
+      }
+    };
+
+    // Map the arguments to the actual listener that was added
+    listenerMap.set({
+      windowId,
+      topic,
+      listener
+    }, receiveMessage);
+
     const [appId, windowName] = windowId.split(':');
 
     if (appId && windowName) {
-      fin.desktop.InterApplicationBus.subscribe(appId, windowName, topic, listener);
+      fin.desktop.InterApplicationBus.subscribe(appId, windowName, topic, receiveMessage);
     } else if (appId) {
-      fin.desktop.InterApplicationBus.subscribe(appId, topic, listener);
+      fin.desktop.InterApplicationBus.subscribe(appId, topic, receiveMessage);
     }
   }
 
   static unsubscribe(windowId: string, topic: string, listener: (...args: any[]) => void) {
-    const [appId, windowName] = windowId.split(':');
+    let deleteKey = null;
+    let receiveMessage = null;
 
-    if (appId && windowName) {
-      fin.desktop.InterApplicationBus.unsubscribe(appId, windowName, topic, listener);
-    } else if (appId) {
-      fin.desktop.InterApplicationBus.unsubscribe(appId, topic, listener);
+    // We cant use listenerMap.has() here because reconstructing the key from the arguments is a different object
+    // I.e. {} !== {}
+    listenerMap.forEach((value, key) => {
+      if (key.windowId === windowId && key.topic === topic && key.listener === listener) {
+        receiveMessage = value;
+        deleteKey = key;
+      }
+    });
+
+    if (deleteKey) {
+      listenerMap.delete(deleteKey);
+
+      const [appId, windowName] = windowId.split(':');
+
+      if (appId && windowName) {
+        fin.desktop.InterApplicationBus.unsubscribe(appId, windowName, topic, receiveMessage);
+      } else if (appId) {
+        fin.desktop.InterApplicationBus.unsubscribe(appId, topic, receiveMessage);
+      }
     }
   }
 }
