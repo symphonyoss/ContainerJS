306497321943d72a9836de3f19c28e2a8abc51a1
Colin Eberhardt <ceberhardt@scottlogic.co.uk>
2017-08-01 15:59:43 +0100

    modified the compatibility layer to work for current Symphony UI

--- packages/api-symphony-compatibility/index.ts
+++ packages/api-symphony-compatibility/index.ts
@@ -5,6 +5,10 @@ const api = (ssf && ssf.registerBoundsChange)
     ? ssf
     : map.ssf;
 
+// the current Symphony UI reuqires that the API is present within the SYM_API namespace, although SymphonyElecrtong supports both
+// https://github.com/symphonyoss/SymphonyElectron/blob/299e75eca328375468cc3d0bf34ae9ca73e445f6/js/preload/preloadMain.js#L229
+(window as any).SYM_API = api;
+
 /* tslint:disable:no-default-export */
 export default api;
 /* tslint:enable:no-default-export */
